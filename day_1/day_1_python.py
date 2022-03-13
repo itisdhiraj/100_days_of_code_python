3f5e0fe8dc1cedcd20e581ae7cdc659ce92ed359
DHIRAJ KUMAR TRIPATHI <itisdhirajtripathi@gmail.com>
2022-03-13 21:54:38 +0100

    Update day_1_python.py

--- day_1/day_1_python.py
+++ day_1/day_1_python.py
@@ -1,1 +1,15 @@
+# Write your code below this line 👇
+
+print("""Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')""")
+
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+
+print("Day 1 - String Manipulation")
+print("""String Concatenation is done with the "+" sign.""")
+print("""e.g. print("Hello " + "world")""")
+print("New lines can be created with a backslash and n.")
+
+print("Hello" + " " + "Dhiraj")
 
