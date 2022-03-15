eebfd5267ce0e24843719e204852ceb88b56c23f
DHIRAJ KUMAR TRIPATHI <itisdhirajtripathi@gmail.com>
2022-03-15 22:58:31 +0100

    Update day_3_python.py

--- day_3/day_3_python.py
+++ day_3/day_3_python.py
@@ -8,7 +8,7 @@ else:
 
 #Write a program that works out whether if a given number is an odd or even number.
 # 🚨 Don't change the code below 👇
-print("Write a program that works out whether if a given number is an odd or even number.")
+print("Program to check whether given number is an odd or even number.")
 number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
@@ -18,6 +18,34 @@ if (number % 2 == 0):
     print("This is an even number.")
 else:
     print("This is an odd number.")
+
+
+# Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+# 🚨 Don't change the code below 👇
+print("Welcome to Body Mass Index (BMI) Calculator.")
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+wt = float(weight)
+ht = float(height)
+bmi_round = wt / (ht * ht)
+# bmi = int(weight) // (int(height) * int(height))
+bmi = round(bmi_round)
+
+#print(bmi)
+if bmi <= 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif 18.5 < bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif 25 <= bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif 30 <= bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
+
 
 
 
