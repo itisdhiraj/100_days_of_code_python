1bd140e1e88beb003e9411a895cdd0ad78f9e681
DHIRAJ KUMAR TRIPATHI <itisdhirajtripathi@gmail.com>
2022-03-15 20:54:52 +0100

    Update day_3_python.py

--- day_3/day_3_python.py
+++ day_3/day_3_python.py
@@ -1,1 +1,25 @@
+#Check the eligiblity to ride rollercoaster
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height > 120:
+  print("You are allow to Ride. Have fun. ")
+else:
+  print("Sorry, Your height is less than or equal to 120 cm. You ared not allow to ride.")
+
+#Write a program that works out whether if a given number is an odd or even number.
+# 🚨 Don't change the code below 👇
+print("Write a program that works out whether if a given number is an odd or even number.")
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if (number % 2 == 0):
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+
+
+
+
 
