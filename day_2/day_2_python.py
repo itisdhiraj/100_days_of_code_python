19ed112a725d1ed2d0f0fdc053d6e5d81d93f595
DHIRAJ KUMAR TRIPATHI <itisdhirajtripathi@gmail.com>
2022-03-15 20:33:30 +0100

    Update day_2_python.py

--- day_2/day_2_python.py
+++ day_2/day_2_python.py
@@ -63,4 +63,23 @@ except ValueError:
     print("Entered value is not a number. Please Enter the two digit integer value")
 
 
+#If the bill was $150.00, split between 5 people, with 12% tip.
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
+print("Welcome to the tip calculator")
+
+bill_amount = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15?"))
+persons = int(input("How many people to split the bill?"))
+
+payment = (bill_amount / persons) * ((100 + tip) / 100)
+tip_round = round(payment, 2)
+print(f"Each person should pay: ${tip_round}")
+
+
 
